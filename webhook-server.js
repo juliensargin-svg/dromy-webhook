907e1234a15c690f6e342cbe1330309af28688cd
Julien <julien@macbookair.home>
2026-04-14 02:51:41 +0200

    fix: supprime railway.json, retire process.exit(1)

--- webhook-server.js
+++ webhook-server.js
@@ -1,6 +1,5 @@
 process.on('uncaughtException', (err) => {
   console.error('[uncaughtException]', err);
-  process.exit(1);
 });
 process.on('unhandledRejection', (reason) => {
   console.error('[unhandledRejection]', reason);
